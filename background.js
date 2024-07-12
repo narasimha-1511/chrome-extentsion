4cab9002ac562c32f74d4362a1a6417f0f26ee8f
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-12 12:18:29 +0530

    added the firebase to the backend
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
+import { initializeApp } from "./firestore/firebase-app.js";
+import { getAuth } from "./firestore/auth.js"; //---> Trying to get the authentication
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request);
   sendResponse("Hello respinse fromt the backend");
 });
+
+
