4b79cc867c5ed8d78ddb7ab3af325916895f3606
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-12 12:09:15 +0530

    added a connection to backend from frnt end

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,13 @@
 console.log("Hello This is narasimha trying to do some stuff");
+
+document.getElementById("savee").addEventListener("click", () => {
+  chrome.runtime.sendMessage(
+    {
+      messsage: "Hello This is narasimha trying to do some stuff",
+    },
+    (response) => {
+      console.log(response);
+      console.log("Message got  an answer");
+    }
+  );
+});
